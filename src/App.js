632ada87583fc7c3ec086e8dd3e321b177f24a69
axelobscura <axosar@gmail.com>
2020-08-24 10:56:55 -0500

    testing decrement button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  decrement = () => {
+    let contador = this.state.counter;
+    if(contador <= 0){
+      alert('No se puede ir abajo de cero')
+    } else {
+      this.setState({ 
+        counter: this.state.counter - 1 
+      })
+    }
+  }
+
   render(){
     return (
       <div data-test="component-app" className="App">
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <button 
+          data-test="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >INCREMENT BUTTON</button>
+        <button 
+          data-test="decrement-button"
+          onClick={this.decrement}
+        >DECREMENT BUTTON</button>
       </div>
     );
   }
