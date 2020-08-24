02c60e342cdedc7b6990da1fcfe6cbac3efc66fb
axelobscura <axosar@gmail.com>
2020-08-24 09:45:20 -0500

    Started testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Learn react testing</h1>
-    </div>
-  );
-}
+class App extends Component {
+  render(){
+    return (
+      <div data-test="component-app" className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+};
 
 export default App;
